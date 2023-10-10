87772d99c8059a789d61998b823f2efdbfd36bc8
pellyadolfo <pellyadolfo@yahoo.es>
2023-10-10 21:30:01 +0200

    added PaymentToken to Factory

--- scripts/deploy-factory.ts
+++ scripts/deploy-factory.ts
@@ -83,17 +83,17 @@ async function main() {
 	cryptocommoditiesFactory.setFacet('ERC20Facet', 1.0, erc20Facet.address);
 
 	// with factory
-	cryptocommoditiesFactory.createCryptocommodity(owner.address, 'TEST');
+	/*cryptocommoditiesFactory.createCryptocommodity(owner.address, 'TEST');
 	let diamondAddress = await cryptocommoditiesFactory.getCryptocommodity(owner.address, 'TEST');
 	console.log('Diamond Address:', diamondAddress)
-	let diamond = await ethers.getContractAt('Diamond', diamondAddress);
+	let diamond = await ethers.getContractAt('Diamond', diamondAddress);*/
 
 	// ***********************************************************************************************************************************************************
 	// *********************************************************** Configure Cryptocommodity Contract ************************************************************
 	// ***********************************************************************************************************************************************************
 	// Attach Facets to Cryptocommodity via DiamondCut in UI
 	// ***********************************************************************************************************************************************************
-	const diamondCutContract = await ethers.getContractAt('DiamondCutFacet', diamond.address)
+	/*const diamondCutContract = await ethers.getContractAt('DiamondCutFacet', diamond.address)
 
 	// attach DiamondLoupeFacet
 	const diamondLoupeFacetSelectors = helpers.getSelectors(diamondLoupeFacet);
@@ -134,6 +134,7 @@ async function main() {
 	// ***********************************************************************************************************************************************************
 	console.log('initializing')
 	console.log(await diamondLoupeContract.facetFunctionSelectors(erc20Facet.address));	 // fetching storage is needed before token.initialize. I do not know why				
+	common.setStorage(helpers.STORAGE1);
 	await token.initialize("CatallacticERC20", "CATA", BigInt(200_000_000 * 10**18));
 	await ico.setPaymentToken("COIN", ico.address, "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419", Math.floor(1100*1e6), 18);
 	console.log('ico.owner()', await ico.owner());
@@ -142,7 +143,7 @@ async function main() {
 	await ico.setTargetWalletAddress(diamond.address);
 	await vesting.addGrantor(ico.address);
 	await diamond.setReceiveFacet(diamond.address);
-	console.log('initialized');
+	console.log('initialized');*/
 
 	// **********************************************************************************************************************************
 	// *************************************************** Configure Default prices *****************************************************
@@ -176,14 +177,12 @@ async function main() {
 		const wbtc = await WBTC.deploy("WBTC", "WBTC");
 		await wbtc.deployed();
 		await wbtc.transfer('0x20caa5fa15c4177cd6946b8041ef40447db27539', ethers.utils.parseUnits("1000000", 18).toString());
-		await ico.setPaymentToken("WBTC", wbtc.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_BTC_IN_USD * 10**6), 18);
 		console.log("WBTC deployed to:", wbtc.address);
 		console.log("WBTC owner balance: " + await wbtc.balanceOf(owner.address));
 		console.log("WBTC 0x20caa5fa15c4177cd6946b8041ef40447db27539 balance: " + await wbtc.balanceOf('0x20caa5fa15c4177cd6946b8041ef40447db27539'));
 
 		// deploy COIN
 		console.log("COIN deploying");
-		await ico.setPaymentToken("COIN", ico.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_ETH_IN_USD * 10**6), 18);
 		console.log("COIN installed");
 		await owner.sendTransaction({
 			to: '0x20caa5fa15c4177cd6946b8041ef40447db27539',
@@ -198,7 +197,6 @@ async function main() {
 		const matic = await MATIC.deploy("MATIC", "MATIC");
 		await matic.deployed();
 		await matic.transfer('0x20caa5fa15c4177cd6946b8041ef40447db27539', ethers.utils.parseUnits("1000000", 18).toString());
-		await ico.setPaymentToken("MATIC", matic.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_MATIC_IN_USD * 10**6), 18);
 		console.log("MATIC deployed to:", matic.address);
 		console.log("MATIC owner balance: " + await matic.balanceOf(owner.address));
 		console.log("MATIC 0x20caa5fa15c4177cd6946b8041ef40447db27539 balance: " + await matic.balanceOf('0x20caa5fa15c4177cd6946b8041ef40447db27539'));
@@ -209,7 +207,6 @@ async function main() {
 		const bnb = await BNB.deploy("BNB", "BNB");
 		await bnb.deployed();
 		await bnb.transfer('0x20caa5fa15c4177cd6946b8041ef40447db27539', ethers.utils.parseUnits("1000000", 18).toString());
-		await ico.setPaymentToken("BNB", bnb.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_BNB_IN_USD * 10**6), 18);
 		console.log("BNB deployed to:", bnb.address);
 		console.log("BNB owner balance: " + await bnb.balanceOf(owner.address));
 		console.log("BNB 0x20caa5fa15c4177cd6946b8041ef40447db27539 balance: " + await bnb.balanceOf('0x20caa5fa15c4177cd6946b8041ef40447db27539'));
@@ -220,10 +217,16 @@ async function main() {
 		const usdt = await USDT.deploy("USDT", "USDT");
 		await usdt.deployed();
 		await usdt.transfer('0x20caa5fa15c4177cd6946b8041ef40447db27539', ethers.utils.parseUnits("1000000", 18).toString());
-		await ico.setPaymentToken("USDT", usdt.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_USDT_IN_USD * 10**6), 18);
 		console.log("USDT deployed to:", usdt.address);
 		console.log("USDT owner balance: " + await usdt.balanceOf(owner.address));
 		console.log("USDT 0x20caa5fa15c4177cd6946b8041ef40447db27539 balance: " + await usdt.balanceOf('0x20caa5fa15c4177cd6946b8041ef40447db27539'));
+
+		// add Payment Tokens
+		await cryptocommoditiesFactory.setPaymentToken("WBTC", wbtc.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_BTC_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("COIN", cryptocommoditiesFactory.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_ETH_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("MATIC", matic.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_MATIC_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("BNB", bnb.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_BNB_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("USDT", usdt.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_USDT_IN_USD * 10**6), 18);
 
 	}
 
@@ -246,14 +249,12 @@ async function main() {
 		const wbtc = await WBTC.deploy("WBTC", "WBTC");
 		await wbtc.deployed();
 		await wbtc.transfer('0x20caa5fa15c4177cd6946b8041ef40447db27539', ethers.utils.parseUnits("1000000", 18).toString());
-		await ico.setPaymentToken("WBTC", wbtc.address, "0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43", Math.floor(DEF_PRICE_BTC_IN_USD * 10**6), 18);
 		console.log("WBTC deployed to:", wbtc.address);
 		console.log("WBTC owner balance: " + await wbtc.balanceOf(owner.address));
 		console.log("WBTC 0x20caa5fa15c4177cd6946b8041ef40447db27539 balance: " + await wbtc.balanceOf('0x20caa5fa15c4177cd6946b8041ef40447db27539'));
 
 		// deploy ETH
 		console.log("COIN deploying");
-		await ico.setPaymentToken("COIN", ico.address, "0x694AA1769357215DE4FAC081bf1f309aDC325306", Math.floor(DEF_PRICE_ETH_IN_USD * 10**6), 18);
 		console.log("COIN installed");
 
 		// deploy MATIC
@@ -262,7 +263,6 @@ async function main() {
 		const matic = await MATIC.deploy("MATIC", "MATIC");
 		await matic.deployed();
 		await matic.transfer('0x20caa5fa15c4177cd6946b8041ef40447db27539', ethers.utils.parseUnits("1000000", 18).toString());
-		await ico.setPaymentToken("MATIC", matic.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_MATIC_IN_USD * 10**6), 18);
 		console.log("MATIC deployed to:", matic.address);
 		console.log("MATIC owner balance: " + await matic.balanceOf(owner.address));
 		console.log("MATIC 0x20caa5fa15c4177cd6946b8041ef40447db27539 balance: " + await matic.balanceOf('0x20caa5fa15c4177cd6946b8041ef40447db27539'));
@@ -273,7 +273,6 @@ async function main() {
 		const bnb = await BNB.deploy("BNB", "BNB");
 		await bnb.deployed();
 		await bnb.transfer('0x20caa5fa15c4177cd6946b8041ef40447db27539', ethers.utils.parseUnits("1000000", 18).toString());
-		await ico.setPaymentToken("BNB", bnb.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_BNB_IN_USD * 10**6), 18);
 		console.log("BNB deployed to:", bnb.address);
 		console.log("BNB owner balance: " + await bnb.balanceOf(owner.address));
 		console.log("BNB 0x20caa5fa15c4177cd6946b8041ef40447db27539 balance: " + await bnb.balanceOf('0x20caa5fa15c4177cd6946b8041ef40447db27539'));
@@ -284,31 +283,29 @@ async function main() {
 		const usdt = await USDT.deploy("USDT", "USDT");
 		await usdt.deployed();
 		await usdt.transfer('0x20caa5fa15c4177cd6946b8041ef40447db27539', ethers.utils.parseUnits("1000000", 18).toString());
-		await ico.setPaymentToken("USDT", usdt.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_USDT_IN_USD * 10**6), 18);
 		console.log("USDT deployed to:", usdt.address);
 		console.log("USDT owner balance: " + await usdt.balanceOf(owner.address));
 		console.log("USDT 0x20caa5fa15c4177cd6946b8041ef40447db27539 balance: " + await usdt.balanceOf('0x20caa5fa15c4177cd6946b8041ef40447db27539'));
+
+		// add Payment Tokens
+		await cryptocommoditiesFactory.setPaymentToken("WBTC", wbtc.address, "0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43", Math.floor(DEF_PRICE_BTC_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("COIN", cryptocommoditiesFactory.address, "0x694AA1769357215DE4FAC081bf1f309aDC325306", Math.floor(DEF_PRICE_ETH_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("MATIC", matic.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_MATIC_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("BNB", bnb.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_BNB_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("USDT", usdt.address, ZERO_ADDRESS, Math.floor(DEF_PRICE_USDT_IN_USD * 10**6), 18);
 
 	}
 
 	if (hre.network.name == 'ethereum') {
 		console.log("deploying to ethereum");
 
-		// deploy WBTC
-		await ico.setPaymentToken("WBTC", '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', '0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c', Math.floor(DEF_PRICE_BTC_IN_USD * 10**6), 18);				
-
-		// deploy ETH
-		await ico.setPaymentToken("COIN", ico.address, "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419", Math.floor(DEF_PRICE_ETH_IN_USD * 10**6), 18);
-
-		// deploy MATIC
-		await ico.setPaymentToken("MATIC", '0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa', "0x7bAC85A8a13A4BcD8abb3eB7d6b4d632c5a57676", Math.floor(DEF_PRICE_MATIC_IN_USD * 10**6), 18);
-
-		// deploy BNB
+		// add Payment Tokens
+		await cryptocommoditiesFactory.setPaymentToken("WBTC", '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', '0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c', Math.floor(DEF_PRICE_BTC_IN_USD * 10**6), 18);				
+		await cryptocommoditiesFactory.setPaymentToken("COIN", cryptocommoditiesFactory.address, "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419", Math.floor(DEF_PRICE_ETH_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("MATIC", '0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa', "0x7bAC85A8a13A4BcD8abb3eB7d6b4d632c5a57676", Math.floor(DEF_PRICE_MATIC_IN_USD * 10**6), 18);
 		// https://etherscan.io/token/0xB8c77482e45F1F44dE1745F52C74426C631bDD52
-		await ico.setPaymentToken("BNB", '0xB8c77482e45F1F44dE1745F52C74426C631bDD52', '0x14e613AC84a31f709eadbdF89C6CC390fDc9540A', Math.floor(DEF_PRICE_BNB_IN_USD * 10**6), 18);
-
-		// deploy USDT
-		await ico.setPaymentToken("USDT", '', '0x3E7d1eAB13ad0104d2750B8863b489D65364e32D', Math.floor(DEF_PRICE_USDT_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("BNB", '0xB8c77482e45F1F44dE1745F52C74426C631bDD52', '0x14e613AC84a31f709eadbdF89C6CC390fDc9540A', Math.floor(DEF_PRICE_BNB_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("USDT", '', '0x3E7d1eAB13ad0104d2750B8863b489D65364e32D', Math.floor(DEF_PRICE_USDT_IN_USD * 10**6), 18);
 
 	}
 
@@ -329,19 +326,16 @@ async function main() {
 		// deploy WBTC
 		// https://mumbai.polygonscan.com/address/0x0d787a4a1548f673ed375445535a6c7a1ee56180
 		// https://github.com/swaponline/MultiCurrencyWallet/blob/master/docs/FAUCETS.md
-		await ico.setPaymentToken("WBTC", '0x0d787a4a1548f673ed375445535a6c7A1EE56180', '0x007A22900a3B98143368Bd5906f8E17e9867581b', Math.floor(DEF_PRICE_BTC_IN_USD * 10**6), 8);
 		console.log("WBTC installed");
 
 		// deploy ETH
 		// https://mumbai.polygonscan.com/token/0xa6fa4fb5f76172d178d61b04b0ecd319c5d1c0aa
 		// https://staging.aave.com/#/faucet
 		// https://faucet.paradigm.xyz/
-		await ico.setPaymentToken("WETH", '0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa', "0x0715A7794a1dc8e42615F059dD6e406A6594651A", Math.floor(DEF_PRICE_ETH_IN_USD * 10**6), 18);
 		console.log("WETH installed");
 
 		// deploy MATIC
 		// https://faucet.polygon.technology/
-		await ico.setPaymentToken("COIN", ico.address, "0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada", Math.floor(DEF_PRICE_MATIC_IN_USD * 10**6), 18);
 		console.log("COIN installed");
 
 		// deploy BNB
@@ -350,7 +344,6 @@ async function main() {
 		const bnb = await BNB.deploy("BNB", "BNB");
 		await bnb.deployed();
 		await bnb.transfer('0x20caa5fa15c4177cd6946b8041ef40447db27539', ethers.utils.parseUnits("1000000", 18).toString());
-		await ico.setPaymentToken("BNB", bnb.address, chainLinkAggregator.address, Math.floor(DEF_PRICE_BNB_IN_USD * 10**6), 18);
 		console.log("BNB deployed to:", bnb.address);
 		console.log("BNB owner balance: " + await bnb.balanceOf(owner.address));
 		console.log("BNB 0x20caa5fa15c4177cd6946b8041ef40447db27539 balance: " + await bnb.balanceOf('0x20caa5fa15c4177cd6946b8041ef40447db27539'));
@@ -359,8 +352,14 @@ async function main() {
 		// deploy USDT
 		// https://mumbai.polygonscan.com/address/0xa02f6adc7926efebbd59fd43a84f4e0c0c91e832
 		// https://calibration-faucet.filswan.com/#/dashboard
-		await ico.setPaymentToken("USDT", '0xa02f6adc7926efebbd59fd43a84f4e0c0c91e832', '0x92C09849638959196E976289418e5973CC96d645', Math.floor(DEF_PRICE_USDT_IN_USD * 10**6), 6);
 		console.log("USDT installed");
+
+		// add Payment Tokens
+		await cryptocommoditiesFactory.setPaymentToken("WBTC", '0x0d787a4a1548f673ed375445535a6c7A1EE56180', '0x007A22900a3B98143368Bd5906f8E17e9867581b', Math.floor(DEF_PRICE_BTC_IN_USD * 10**6), 8);
+		await cryptocommoditiesFactory.setPaymentToken("WETH", '0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa', "0x0715A7794a1dc8e42615F059dD6e406A6594651A", Math.floor(DEF_PRICE_ETH_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("COIN", cryptocommoditiesFactory.address, "0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada", Math.floor(DEF_PRICE_MATIC_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("BNB", bnb.address, chainLinkAggregator.address, Math.floor(DEF_PRICE_BNB_IN_USD * 10**6), 18);
+		await cryptocommoditiesFactory.setPaymentToken("USDT", '0xa02f6adc7926efebbd59fd43a84f4e0c0c91e832', '0x92C09849638959196E976289418e5973CC96d645', Math.floor(DEF_PRICE_USDT_IN_USD * 10**6), 6);
 
 	}
 
@@ -370,39 +369,45 @@ async function main() {
 		// checks for gas because ethersjs has gas hardcoded and this gives transaction underpriced error
 		// https://github.com/ethers-io/ethers.js/issues/2828
 		// https://github.com/ethers-io/ethers.js/issues/3370
-		const gasEstimated = await ico.estimateGas.setPaymentToken("FOOBAR", '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6', '0xDE31F8bFBD8c84b5360CFACCa3539B938dd78ae6', Math.floor(DEF_PRICE_BTC_IN_USD * 10**6), 18);
+		const gasEstimated = await cryptocommoditiesFactory.estimateGas.setPaymentToken("FOOBAR", '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6', '0xDE31F8bFBD8c84b5360CFACCa3539B938dd78ae6', Math.floor(DEF_PRICE_BTC_IN_USD * 10**6), 18);
 		console.log("gasEstimated:", gasEstimated);
 		const GAS = await calcGas(gasEstimated);
-		console.log("gas:", GAS);
+		//console.log("gas:", GAS);
 
 		// deploy WBTC
 		// https://polygonscan.com/token/0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6
-		const tx1 = await ico.setPaymentToken("WBTC", '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6', '0xDE31F8bFBD8c84b5360CFACCa3539B938dd78ae6', Math.floor(DEF_PRICE_BTC_IN_USD * 10**6), 18, GAS);
-		await tx1.wait();
 		console.log("WBTC installed");
 
 		// deploy ETH
 		// https://polygonscan.com/token/0x7ceb23fd6bc0add59e62ac25578270cff1b9f619
-		const tx2 = await ico.setPaymentToken("WETH", '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', "0xF9680D99D6C9589e2a93a78A04A279e509205945", Math.floor(DEF_PRICE_ETH_IN_USD * 10**6), 18, GAS);
-		await tx2.wait();
+
 		console.log("WETH installed");
 
 		// deploy MATIC
-		const tx3 = await ico.setPaymentToken("COIN", ico.address, "0xAB594600376Ec9fD91F8e885dADF0CE036862dE0", Math.floor(DEF_PRICE_MATIC_IN_USD * 10**6), 18, GAS);
-		await tx3.wait();
+
 		console.log("COIN installed");
 
 		// deploy BNB
 		// https://polygonscan.com/token/0x3BA4c387f786bFEE076A58914F5Bd38d668B42c3
-		const tx4 = await ico.setPaymentToken("BNB", '0x3BA4c387f786bFEE076A58914F5Bd38d668B42c3', '0x82a6c4AF830caa6c97bb504425f6A66165C2c26e', Math.floor(DEF_PRICE_BNB_IN_USD * 10**6), 18, GAS);
-		await tx4.wait();
+
 		console.log("BNB installed");
 
 		// deploy USDT
 		// https://polygonscan.com/token/0xc2132d05d31c914a87c6611c10748aeb04b58e8f
-		const tx5 = await ico.setPaymentToken("USDT", '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', '0x0A6513e40db6EB1b165753AD52E80663aeA50545', Math.floor(DEF_PRICE_USDT_IN_USD * 10**6), 18, GAS);
-		await tx5.wait();
+
 		console.log("USDT installed");
+
+		// add Payment Tokens
+		const tx1 = await cryptocommoditiesFactory.setPaymentToken("WBTC", '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6', '0xDE31F8bFBD8c84b5360CFACCa3539B938dd78ae6', Math.floor(DEF_PRICE_BTC_IN_USD * 10**6), 18, GAS);
+		await tx1.wait();
+		const tx2 = await cryptocommoditiesFactory.setPaymentToken("WETH", '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619', "0xF9680D99D6C9589e2a93a78A04A279e509205945", Math.floor(DEF_PRICE_ETH_IN_USD * 10**6), 18, GAS);
+		await tx2.wait();
+		const tx3 = await cryptocommoditiesFactory.setPaymentToken("COIN", cryptocommoditiesFactory.address, "0xAB594600376Ec9fD91F8e885dADF0CE036862dE0", Math.floor(DEF_PRICE_MATIC_IN_USD * 10**6), 18, GAS);
+		await tx3.wait();
+		const tx4 = await cryptocommoditiesFactory.setPaymentToken("BNB", '0x3BA4c387f786bFEE076A58914F5Bd38d668B42c3', '0x82a6c4AF830caa6c97bb504425f6A66165C2c26e', Math.floor(DEF_PRICE_BNB_IN_USD * 10**6), 18, GAS);
+		await tx4.wait();
+		const tx5 = await cryptocommoditiesFactory.setPaymentToken("USDT", '0xc2132d05d31c914a87c6611c10748aeb04b58e8f', '0x0A6513e40db6EB1b165753AD52E80663aeA50545', Math.floor(DEF_PRICE_USDT_IN_USD * 10**6), 18, GAS);
+		await tx5.wait();
 
 	}
 
